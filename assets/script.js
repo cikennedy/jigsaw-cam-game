a9f8c134ed7641acee8b727eff0e407e6f7c36d9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-04 14:59:58 -0500

    created function to initialize puzzle pieces, defined the puzzle piece class and constructor

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,9 @@ let CONTEXT = null;
 // specify how much of the screen space will be used by the image. adjusting this will change the margin of the cam image
 let SCALER = 0.8;
 // keep track of other related information in the size variable
-let SIZE = { x: 0, y: 0, width: 0, height: 0 };
+let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3};
+// array of pieces 
+let PIECES = [];
 
 const main = () => {
   CANVAS = document.getElementById("myCanvas");
@@ -27,8 +29,8 @@ const main = () => {
 
       // when video data is available, we can update it on the canvas
       VIDEO.onloadeddata = function () {
-          handleResize();
-        window.addEventListener("resize", handleResize);
+        handleResize();
+        // window.addEventListener("resize", handleResize);
         updateCanvas();
       };
       // define errors if camera not allowed or if another error arises
@@ -39,9 +41,9 @@ const main = () => {
 };
 
 const handleResize = () => {
-    // canvas will resize, not just the camera stream 
-    CANVAS.width = window.innerWidth;
-    CANVAS.height = window.innerHeight;
+  // canvas will resize, not just the camera stream
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
   // update scaler/size values here when metadata is available
   let resizer =
     SCALER *
@@ -65,3 +67,27 @@ const updateCanvas = () => {
   // requestAnimationFrame method will call the function recursively many times per second to provide a live image
   window.requestAnimationFrame(updateCanvas);
 };
+
+const initializePieces = () => {
+    PIECES = [];
+    // iterate through the rows using i, rows using j
+    for (let i=0; i<SIZE.rows; i++) {
+        for (let j=0; j<SIZE.columns; j++) {
+            // add a new piece using these two indeces 
+            PIECES.push(new Piece(i,j));
+        }
+    }
+}
+
+// define piece class, specify a row and column index in constructor
+class Piece{
+    constructor(rowIndex, colIndex) {
+        this.rowIndex=rowIndex;
+        this.colIndex=colIndex;
+    }
+    // to be able to draw the pieces, use draw method using context as a parameter
+    draw(context) {
+        context.beginPath();
+        context.rect()
+    }
+}
