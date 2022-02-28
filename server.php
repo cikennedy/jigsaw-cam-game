257f5c87bc8ca668e2cca1acd8c091a66bd216fe
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-02-28 17:32:54 -0600

    added function for returning results for each difficulty level of the  game

--- server.php
+++ server.php
@@ -7,6 +7,42 @@ $db_password="";
 if(!$lnk)
     // die function terminates php code 
     die("Database connection failed");
-echo "Database connection successful";
 
+mysqli_select_db($lnk, "puzzlecam") or die ("failed to select DB");
+
+// scores for each difficulty level using their difficulty as a parameter of the function 
+$easy=getScoresWithDifficulty("Easy", $lnk);
+print_r($easy);
+$medium=getScoresWithDifficulty("Medium", $lnk);
+print_r($medium);
+$hard=getScoresWithDifficulty("Hard", $lnk);
+print_r($hard);
+$impossible=getScoresWithDifficulty("Impossible", $lnk);
+print_r($impossible);
+
+// select entries from scorer's table by difficulty
+// . is used for concatanation in php
+function getScoresWithDifficulty($difficulty){
+    $query="Select Name, Time FROM Scores". 
+        " WHERE Difficulty Like 'Easy'".
+        " ORDER BY Time";
+
+    // execute the query using the mysqli_query function
+    // this function doesn't actually return the result, 
+    // but an object that we can use to iterate through the reults
+    $rs=mysqli_query($lnk,$query);
+
+    // we first initialize an empty array
+    $results=array();
+    // then if the result contains any rows, we use a while loop 
+    // to fetch each row one by one using the mysqli_fetch_assoc function on the rs object
+    if(mysqli_num_rows($rs)>0){
+        while($row=mysqli_fetch_assoc($rs)){
+            // we add each row to the results array using the array_push function
+            array_push($results,$row);
+        }
+    }
+
+    return $results;
+}
 ?>
