772120cceca9e8cbab607566167e53de390ea590
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-02-28 17:19:02 -0600

    changes to server.php file to kill php code if connection fails

--- server.php
+++ server.php
@@ -3,5 +3,10 @@
 $db_host="localhost";
 $db_user="root";
 $db_password="";
+// if link not established, we warn that the connection failed 
+if(!$lnk)
+    // die function terminates php code 
+    die("Database connection failed");
+echo "Database connection successful";
 
 ?>
